74b76930fc6ad90440091acfbc0c9c5674fbce9d
Michael Mburu <ichangaimichael@yahoo.com>
2024-03-30 10:47:29 +0300

    - Added Node

--- Main.py
+++ Main.py
@@ -6,41 +6,12 @@ from Block import Block
 from Blockchain import Blockchain
 from Utils import Utils
 from AccountModel import AccountModel
+from Node import Node
 
 if __name__ == '__main__':
+	node  = Node()
+	print(node.blockchain)
+	print(node.transactionPool)
+	print(node.wallet)
 
-  blockChain = Blockchain()
-  pool = TransactionPool()
-
-  alice = Wallet()
-  bob = Wallet()
-  exchange = Wallet()
-  forger = Wallet()
-
-  exchangeTransaction = exchange.createTransaction(alice.publicKeyString(), 10, 'EXCHANGE')
-
-  if not pool.transactionExists(exchangeTransaction):
-    pool.addTransaction(exchangeTransaction)
-
-  coveredTransaction = blockChain.getCoveredTransactions(pool.transactions)
-  lastHash = Utils.hash(blockChain.blocks[-1].payload()).hexdigest()
-  blockCount = blockChain.blocks[-1].blockCount + 1
-  blockOne = forger.createBlock(coveredTransaction, lastHash, blockCount)
-  blockChain.addBlock(blockOne)
-
-  #Alice wants to send 5 tokens to bob
-  transaction = alice.createTransaction(bob.publicKeyString(), 5, 'TRANSFER')
-
-  if not pool.transactionExists(transaction):
-    pool.addTransaction(transaction)
-
-  coveredTransaction = blockChain.getCoveredTransactions(pool.transactions)
-  lastHash = Utils.hash(blockChain.blocks[-1].payload()).hexdigest()
-  blockCount = blockChain.blocks[-1].blockCount + 1
-  blockTwo = forger.createBlock(coveredTransaction, lastHash, blockCount)
-  blockChain.addBlock(blockTwo)
-
-
-  pprint.pprint(blockChain.toJson())
-  
-
+	
